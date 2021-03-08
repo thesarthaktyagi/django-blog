c581e74bfc8368b6b82a3dc169b7233c774343d7
Sarthak Tyagi <srthk07@gmail.com>
2021-03-08 13:58:08 +0530

    Sitemaps done

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from apps.blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('apps.blog.urls', namespace='apps.blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 
 ]
